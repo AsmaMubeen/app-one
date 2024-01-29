1941070dc7ff0b41334a0ed50e624a202a043147
Asma <nc09@LNT-32.local>
2024-01-29 14:35:00 +0530

    changes because of trying out new challenges

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Tacha Development. All rights reserved.</small>
+        </footer>
+
+    )
+}
+
+export default Footer;
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+import logo from '../logo.svg';
+
+function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src={logo} alt="logo" width='50px'></img>
+            </nav>
+        </header>
+
+    )
+}
+
+export default Navbar;
